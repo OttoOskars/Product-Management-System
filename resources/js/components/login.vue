5cb9f480ea0a9cedce165e7abd646f82c5bdacc0
Markuss Spricis <markuss5764@gmail.com>
2023-10-02 13:29:31 +0300

    changed css

--- resources/js/components/login.vue
+++ resources/js/components/login.vue
@@ -1,15 +1,40 @@
 <template>
-    <div id="shadowBox">
-      <h3 class="rainbow rainbow_text_animated">HELLO THERE!</h3>
-      <h3 class="rainbow rainbow_text_animated">TWITTER</h3>
+  <div>
+    <div>
+      <h1 class="welcome">Welcome</h1>
     </div>
+    <div>
+      <h2 class="join_today">Join today</h2>
+    </div>
+    <div>
+      <div class="container">
+        <button class="google-signup">Sign up with Google</button>
+      </div>
+      <div class="container">
+        <button class="apple-signup">Sign up with Apple</button>
+      </div>
+      <div class="line">
+        <div class="left"></div>
+        <span class="text">or</span>
+        <div class="right"></div>
+      </div>
+      <div class="container">
+        <button class="create-account">Create account</button>
+      </div>
+      <div class="container">
+        <p class="text">Already have an account?</p>
+      </div>
+      <div class="container">
+        <button class="sign-in">Sign in</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'login',
+};
+</script>
+<style lang="scss" scoped>
     
-  </template>
-  <script>
-  export default {
-    name: 'Login',
-  };
-  </script>
-  <style lang="scss" scoped>
-      
-  </style>
+</style>
