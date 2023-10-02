eeca6d5cce5c2235c747bb536e5937d3eb490b48
Markuss Spricis <markuss5764@gmail.com>
2023-10-02 14:58:20 +0300

    Made login page

--- resources/js/components/login.vue
+++ resources/js/components/login.vue
@@ -1,34 +1,59 @@
 <template>
 
-    <div id="shadowBox">
-      <h3 class="rainbow rainbow_text_animated">HELLO THERE!</h3>
-      <h3 class="rainbow rainbow_text_animated">TWITTER</h3>
-      <h3>{{ counter }}</h3>
-    </div>
+  <div id="shadowBox">
+    <h3 class="rainbow rainbow_text_animated">HELLO THERE!</h3>
+    <h3 class="rainbow rainbow_text_animated">TWITTER</h3>
+    <!-- <h3>{{ counter }}</h3> -->
+  </div>
 
-    <button @click="counter++">+</button>
-    <button @click="counter--">-</button>
-    <template v-if="counter > 0">
-        <input  :value="input" type="text" @input="onInput">
-    </template>
+    <div>
+      <div>
+        <h1 class="welcome">Welcome</h1>
+      </div>
+      <div>
+        <h2 class="join_today">Join today</h2>
+      </div>
+      <div>
+        <div class="container">
+          <button class="google-signup">Sign up with Google</button>
+        </div>
+        <div class="container">
+          <button class="apple-signup">Sign up with Apple</button>
+        </div>
+        <div class="line">
+          <div class="left"></div>
+          <span class="text">or</span>
+          <div class="right"></div>
+        </div>
+        <div class="container">
+          <button class="create-account">Create account</button>
+        </div>
+        <div class="container">
+          <p class="text">Already have an account?</p>
+        </div>
+        <div class="container">
+          <button class="sign-in">Sign in</button>
+        </div>
+      </div>
+    </div>
   </template>
   <script>
   export default {
     name: 'login',
-    data() {
-      return {
-        input: null,
-        counter: 0,
-      }
-    },
-    methods: {
-      onInput() {
-        this.counter++;
-      },
-      decrement() {
-        this.counter--;
-      },
-    },
+    // data() {
+    //   return {
+    //     input: null,
+    //     counter: 0,
+    //   }
+    // },
+    // methods: {
+    //   onInput() {
+    //     this.counter++;
+    //   },
+    //   decrement() {
+    //     this.counter--;
+    //   },
+    // },
   };
   </script>
   <style lang="scss" scoped>
