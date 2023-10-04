7e0ca073cda36208aa688b267fb1de1fc3040579
Otto Oskars Āboliņš <ottooskarsabolins@gmail.com>
2023-10-04 16:08:21 +0300

    Login changes

---
+++ resources/js/components/Popup.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="pop-up">
+        <div class="pop-up-inner">
+            <ion-icon name="logo-twitter" class="pop-up-icon"></ion-icon>
+            <div class="pop-up-close-div">
+                <button class="pop-up-close" @click="TogglePopup()"><ion-icon name="close" class="close"></ion-icon></button>
+            </div>
+            <slot></slot>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Popup',
+    props: ['TogglePopup'],
+}
+</script>
+<style lang="scss" scoped>
+.pop-up {
+    font-family: Arial, sans-serif;
+    position:fixed;
+    top:0;
+    bottom:0;
+    left:0;
+    right:0;
+    z-index: 99;
+    background:rgba(29, 161, 242, 0.1);
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    .pop-up-inner {
+        background:#000000;
+        padding:20px 70px;
+        border-radius:20px;
+        display:flex;
+        flex-direction:column;
+        gap:40px;
+        justify-content: center;
+        position: relative;
+        .pop-up-close-div {
+            position: absolute;
+            top:10px;
+            left:10px;
+        }
+        .pop-up-close {
+            width: 30px;
+            height: 30px;
+            border-radius: 50%;
+            background-color: #000;
+            display:flex;
+            justify-content: center;
+            align-items: center;
+            border: none;
+            cursor: pointer;
+        }
+        .pop-up-close:hover{
+            background-color: #202020;
+        }
+        .pop-up-icon{
+            color:white;
+            position:absolute;
+            font-size:30px;
+            top:5px;
+            left:50%;
+            transform: translate(-50%);
+        }
+    }
+}
+.close{
+    font-size: 20px;
+    color:white;
+}
+</style>
--- resources/js/components/login.vue
+++ resources/js/components/login.vue
@@ -30,19 +30,76 @@
         <p class="text">Already have an account?</p>
       </div>
       <div class="container">
-        <button class="sign-in">Sign in</button>
+        <button class="sign-in" @click="() => TogglePopup('SignInTrigger')">Sign in</button>
       </div>
     </div>
   </div>
+  <Popup v-if="popupTriggers.SignInTrigger" :TogglePopup="() => TogglePopup('SignInTrigger')">
+    <div class="Sign-Pop-Up">
+      <h1 class="Bold-text">Sign in to Twitter</h1>
+      <div class="input-wrap">
+        <input type="text" id="sign-in-email" class="SignInput" required>
+        <label for="sign-in-email">Email</label>
+      </div>
+      <button class="FormButton" @click="() => nextSignIn()">Next</button>
+      <p style="color:#434343; font-size:small">Don't have an account? <button class="sign-link-button" @click="() => closeSignInOpenSignUp()">Sign up</button></p>
+    </div>
+  </Popup>
+  <Popup v-if="popupTriggers.SignIn2Trigger" :TogglePopup="() => TogglePopup('SignIn2Trigger')">
+    <div class="Sign-Pop-Up">
+      <h1 class="Bold-text">Password</h1>
+      <div class="input-wrap disabled-input">
+        <input type="text" class="SignInput" id="sign-in-email-disabled" value="example@gmail.com" required disabled>
+        <label for="sign-in-email-disabled">Email</label>
+      </div>
+      <div class="input-wrap">
+        <input type="password" id="sign-in-pass" class="SignInput" required>
+        <label for="sign-in-pass">Password</label>
+      </div>
+      <button class="FormButton">Log in</button>
+      <p style="color:#434343; font-size:small">Don't have an account? <button class="sign-link-button" @click="() => closeSignInOpenSignUp()">Sign up</button></p>
+    </div>
+  </Popup>
   <create-account v-if="showCreateAccount" @close-page="toggleShowAccount" />
 </template>
 <script>
+import { ref } from 'vue';
 import CreateAccount from './create_account.vue'
+import Popup from './Popup.vue';
+import DOB from './dob.vue';
 export default {
   name: 'login',
   components: {
     CreateAccount,
+    Popup,
+    DOB
   },
+  setup () {
+		const popupTriggers = ref({
+			buttonTrigger: false,
+      SignInTrigger:false,
+		});
+
+    const nextSignIn = () => {
+      popupTriggers.value.SignInTrigger=false;
+      popupTriggers.value.SignIn2Trigger=true;
+    }
+
+    const closeSignInOpenSignUp = () => {
+      popupTriggers.value.SignInTrigger = false;
+      popupTriggers.value.SignUpTrigger = true;
+    }
+		const TogglePopup = (trigger) => {
+			popupTriggers.value[trigger] = !popupTriggers.value[trigger]
+		}
+		return {
+			Popup,
+			popupTriggers,
+      closeSignInOpenSignUp,
+			TogglePopup,
+      nextSignIn
+		}
+	},
   data: () => ({
     showCreateAccount: false,
   }),
@@ -54,9 +111,179 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-  .apple-logo{
-    font-size:27.5px;
-  }
+.apple-logo{
+  font-size:27.5px;
+}
+.login-text {
+  text-align: center;
+  color: white;
+  font-family: Arial, Helvetica, sans-serif;
 
+}
+.text {
+  font-family: Arial, Helvetica, sans-serif;
+  padding: 0 10px;
+  color: white;
+}
+.sign-up-btn {
+  font-family: Arial, Helvetica, sans-serif;
+  background-color: white;
+  border: none;
+  border-radius: 50px;
+  color: #333;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  margin: 4px 2px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 400px;
+  cursor: pointer;
+  span{margin-right: 10px;}
+  img{margin-right: 10px;}
+}
+.container {
+  text-align: center;
+}
+.line {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 400px;
+  margin: 10px auto;
+  .left, .right {
+    flex: 1;
+    height: 1px;
+    background-color: lightgray;
+  }
+}
+.create-account {
+  font-family: Arial, Helvetica, sans-serif;
+  background-color: blue;
+  border: none;
+  border-radius: 50px;
+  color: white;
+  font-weight: bold;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 14px;
+  margin: 4px 2px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 400px;
+  cursor: pointer;
+}
+.sign-in {
+  font-family: Arial, Helvetica, sans-serif;
+  background-color: black;
+  border: 1px solid blue;
+  border-radius: 50px;
+  color: blue;
+  font-weight: bold;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 14px;
+  margin: 4px 2px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 400px;
+  cursor: pointer;
+}
+.FormButton{
+  font-family: Arial, Helvetica, sans-serif;
+  background-color: white;
+  border: none;
+  border-radius: 50px;
+  color: #333;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  width: 100%;
+  cursor: pointer;
+  span{margin-right: 10px;}
+}
+
+.Sign-Pop-Up{
+  display:flex;
+  flex-direction:column;
+  justify-content: center;
+  width:400px;
+  gap:15px;
+  margin-top:10px;
+}
+.SignInput{
+  border:none;
+  color:#ffffff;
+  padding: 16px 12px;
+  font-size:medium;
+  outline:none;
+  background: none;
+  position:relative;
+  top:8px;
+  width:100%;
+}
+.SignInput:disabled{
+  color:#434343;
+}
+
+.input-wrap{
+  border: 1px solid #434343;
+  border-radius:6px;
+  font-family: Arial, sans-serif;
+  position:relative;
+  width:auto;
+  height: auto;
+  label{
+    font-size:medium;
+    color:#434343;
+    padding:5px;
+    position:absolute;
+    top:11px;
+    left:5px;
+    pointer-events: none;
+    transition: 0.3s ease all;
+  }
+}
+.disabled-input{
+  background-color:rgba(34, 34, 34,0.4);
+  border:none;
+}
+.FormButton{
+  margin-bottom:10px;
+  font-weight:bold;
+}
+.SignInput:focus+label,
+.SignInput:disabled+label,
+.SignInput:valid+label{
+  font-size:small;
+  top:3px;
+  left:7px;
+  padding:0 5px 0 5px;
+}
+.sign-link-button{
+  border:none;
+  background:none;
+  color:#1da1f2;
+  padding:0;
+}
+.Bold-text{
+  font-weight: bold;
+  color:white;
+}
+.DateofBirth{
+  margin-bottom:20px;
+}
 </style>
 
