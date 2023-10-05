40176a3a04c3d2cbca70b783a2e0cfadfddfa8c1
Markuss Spricis <markuss5764@gmail.com>
2023-10-05 16:12:59 +0300

    Create account - done

--- resources/js/components/create_account.vue
+++ resources/js/components/create_account.vue
@@ -1,149 +1,120 @@
 <template>
 
-    <div class="account-container">
-        <div class="X-2">
-            <button class="X" @click="$emit('close-page')"><ion-icon name="close" class="close"></ion-icon></button>
-        </div>
-        <div class="heading_create">
-            <h1>Create your account</h1>
-        </div>
-        <div>
-            <div class="name">
-                <div class="input-container">
-                    <input
-                        type="text"
-                        maxlength="50"
-                        class="account-input"
-                        style="padding-left: 10px;"
-                        v-model="name"
-                        @input="updateLabel('name')"
-                        @focus="moveLabelUp('name')"
-                        @blur="resetLabelPosition('name')"
-                    />
-                    <label class="input-label" :class="{ active: isLabelActive['name'], committed: isInputCommitted['name'] }">Name</label>
-                </div>
-            </div>
-
-            <div class="email">
-                <div class="input-container">
-                    <input
-                        type="email"
-                        maxlength="50"
-                        class="account-input"
-                        style="padding-left: 10px;"
-                        v-model="email"
-                        @input="updateLabel('email')"
-                        @focus="moveLabelUp('email')"
-                        @blur="resetLabelPosition('email')"
-                    />
-                    <label class="input-label" :class="{ active: isLabelActive['email'], committed: isInputCommitted['email'] }">Email</label>
-                </div>
-                <p v-if="invalidEmail" class="warning-1">Please enter a valid email address.</p>
-            </div>
-
-            <div class="password">
-                <div class="input-container">
-                    <input
-                        type="password"
-                        maxlength="50"
-                        class="account-input"
-                        style="padding-left: 10px;"
-                        v-model="password"
-                        @input="updateLabel('password')"
-                        @focus="moveLabelUp('password')"
-                        @blur="resetLabelPosition('password')"
-                    />
-                    <label class="input-label" :class="{ active: isLabelActive['password'], committed: isInputCommitted['password'] }">Password</label>
-                </div>
-            </div>
-
-            <div class="confirm_password">
-                <div class="input-container">
-                    <input
-                        type="password"
-                        maxlength="50"
-                        class="account-input"
-                        style="padding-left: 10px;"
-                        v-model="confirmPassword"
-                        @input="updateLabel('confirmPassword')"
-                        @focus="moveLabelUp('confirmPassword')"
-                        @blur="resetLabelPosition('confirmPassword')"
-                    />
-                    <label class="input-label" :class="{ active: isLabelActive['confirmPassword'], committed: isInputCommitted['confirmPassword'] }">Confirm Password</label>
-                </div>
-                <p v-if="passwordsDoNotMatch" class="warning-2">Passwords do not match.</p>
-            </div>
-
-            <div class="birth_date">
-                <h3>Date of birth</h3>
-                <p class="p">This will not be shown publicly. Confirm your own age, even if this account is for a business, a pet, or something else.</p>
-            </div>
-            <div>
-                <div class="months">
-                    <select class="select-1" v-model="month" ref="selectEl">
-                        <option value="" disabled selected>Month</option>
-                        <option value="January">January</option>
-                        <option value="February">February</option>
-                        <option value="March">March</option>
-                        <option value="April">April</option>
-                        <option value="May">May</option>
-                        <option value="June">June</option>
-                        <option value="July">July</option>
-                        <option value="August">August</option>
-                        <option value="September">September</option>
-                        <option value="October">October</option>
-                        <option value="November">November</option>
-                        <option value="December">December</option>
-                    </select>
-                </div>
-                <div class="days">
-                    <select class="select-2" v-model="day" ref="selectDay">
-                        <option value="" disabled selected>Day</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
-                        <option value="13">13</option>
-                        <option value="14">14</option>
-                        <option value="15">15</option>
-                        <option value="16">16</option>
-                        <option value="17">17</option>
-                        <option value="18">18</option>
-                        <option value="19">19</option>
-                        <option value="20">20</option>
-                        <option value="21">21</option>
-                        <option value="22">22</option>
-                        <option value="23">23</option>
-                        <option value="24">24</option>
-                        <option value="25">25</option>
-                        <option value="26">26</option>
-                        <option value="27">27</option>
-                        <option value="28">28</option>
-                        <option value="29">29</option>
-                        <option value="30">30</option>
-                        <option value="31">31</option>
-                    </select>
-                </div>
-                <div class="years">
-                    <select class="select-3" v-model="year" ref="selectYear">
-                        <option value="" disabled selected>Year</option>
-                        <option id="year" v-for="year in years" :key="year" :value="year">{{ year }}</option>
-                    </select>
-                </div>
-            </div>
-            <div>
-                <button class="next" @click="validateForm" v-bind:disabled="!allFieldsFilled">Next</button>
-            </div>
-        </div>
+  <div class="account-container">
+    <div class="X-2">
+        <button class="X" @click="$emit('close-page')"><ion-icon name="close" class="close"></ion-icon></button>
     </div>
+    <div class="heading_create">
+        <h1>Create your account</h1>
+    </div>
+    <div>
+      <div class="name">
+        <div class="input-container">
+          <input
+            type="text"
+            maxlength="50"
+            class="account-input"
+            style="padding-left: 10px;"
+            v-model="name"
+            @input="updateLabel('name')"
+            @focus="moveLabelUp('name')"
+            @blur="resetLabelPosition('name')"
+          />
+          <label class="input-label" :class="{ active: isLabelActive['name'], committed: isInputCommitted['name'] }">Name</label>
+        </div>
+        <p v-if="nameHasSpaces" class="warning-3">Name should not contain spaces.</p>
+      </div>
+
+      <div class="email">
+        <div class="input-container">
+          <input
+              type="email"
+              maxlength="50"
+              class="account-input"
+              style="padding-left: 10px;"
+              v-model="email"
+              @input="updateLabel('email')"
+              @focus="moveLabelUp('email')"
+              @blur="resetLabelPosition('email')"
+          />
+          <label class="input-label" :class="{ active: isLabelActive['email'], committed: isInputCommitted['email'] }">Email</label>
+        </div>
+        <p v-if="invalidEmail" class="warning-1">Please enter a valid email address.</p>
+      </div>
+
+      <div class="password">
+        <div class="input-container">
+            <input
+                type="password"
+                maxlength="50"
+                class="account-input"
+                style="padding-left: 10px;"
+                v-model="password"
+                @input="updateLabel('password')"
+                @focus="moveLabelUp('password')"
+                @blur="resetLabelPosition('password')"
+            />
+            <label class="input-label" :class="{ active: isLabelActive['password'], committed: isInputCommitted['password'] }">Password</label>
+        </div>
+      </div>
+
+      <div class="confirm_password">
+        <div class="input-container">
+            <input
+                type="password"
+                maxlength="50"
+                class="account-input"
+                style="padding-left: 10px;"
+                v-model="confirmPassword"
+                @input="updateLabel('confirmPassword')"
+                @focus="moveLabelUp('confirmPassword')"
+                @blur="resetLabelPosition('confirmPassword')"
+            />
+            <label class="input-label" :class="{ active: isLabelActive['confirmPassword'], committed: isInputCommitted['confirmPassword'] }">Confirm Password</label>
+        </div>
+        <p v-if="passwordsDoNotMatch" class="warning-2">Passwords do not match.</p>
+      </div>
+
+      <div class="birth_date">
+        <h3>Date of birth</h3>
+        <p class="p">This will not be shown publicly. Confirm your own age, even if this account is for a business, a pet, or something else.</p>
+      </div>
+      <div>
+        <div class="months">
+          <select class="select-1" v-model="month" ref="selectEl">
+              <option value="" disabled selected>Month</option>
+              <option value="January">January</option>
+              <option value="February">February</option>
+              <option value="March">March</option>
+              <option value="April">April</option>
+              <option value="May">May</option>
+              <option value="June">June</option>
+              <option value="July">July</option>
+              <option value="August">August</option>
+              <option value="September">September</option>
+              <option value="October">October</option>
+              <option value="November">November</option>
+              <option value="December">December</option>
+          </select>
+        </div>
+        <div class="days">
+          <select class="select-2" v-model="day" ref="selectDay">
+              <option value="" disabled selected>Day</option>
+              <option v-for="day in daysInMonth" :key="day" :value="day">{{ day }}</option>
+          </select>
+        </div>
+        <div class="years">
+          <select class="select-3" v-model="year" ref="selectYear">
+              <option value="" disabled selected>Year</option>
+              <option id="year" v-for="year in years" :key="year" :value="year">{{ year }}</option>
+          </select>
+        </div>
+      </div>
+      <div>
+        <button class="next" @click="validateForm" v-bind:disabled="!allFieldsFilled">Create account</button>
+      </div>
+    </div>
+  </div>
 
 </template>
 
@@ -160,6 +131,7 @@ export default {
       day: '',
       year: '',
       years: Array.from({length: 121}, (_, i) => (1903 + i)),
+      nameHasSpaces: false,
       invalidEmail: false,
       passwordsDoNotMatch: false,
       isLabelActive: {
@@ -180,6 +152,18 @@ export default {
     allFieldsFilled() {
       return this.name && this.email && this.password && this.confirmPassword && this.month && this.day && this.year;
     },
+    daysInMonth() {
+      if (["April", "June", "September", "November"].includes(this.month)) {
+        return Array.from({ length: 30 }, (_, i) => i + 1);
+      } else if (this.month === "February") {
+        return Array.from({ length: 28 }, (_, i) => i + 1);
+      } else {
+        return Array.from({ length: 31 }, (_, i) => i + 1);
+      }
+    },
+    // nameHasSpaces() {
+    //   return this.name.includes(' ');
+    // },
   },
   mounted() {
     document.addEventListener('click', this.handleClickOutside);
@@ -200,11 +184,21 @@ export default {
     year: function () {
       this.$refs.selectYear.style.color = "white";
     },
+    selectedMonth() {
+      this.day = '';
+    },
   },
   methods: {
     validateForm() {
+      this.nameHasSpaces = false;
       this.invalidEmail = false;
       this.passwordsDoNotMatch = false;
+
+      if (this.name.includes(' ')) { // Check for spaces in the name
+        this.nameHasSpaces = true;
+        setTimeout(() => { this.nameHasSpaces = false; }, 3000); // Show for 3 seconds
+        return;
+      }
 
       if (!this.email.includes('@')) {
         this.invalidEmail = true;
@@ -259,7 +253,15 @@ export default {
 
     .warning-2 {
         position: absolute;
-        top: 100%;
+        top: 80%;
+        left: 0;
+        color: red;
+        font-size: 12px;
+    }
+
+    .warning-3 {
+        position: absolute;
+        top: 80%;
         left: 0;
         color: red;
         font-size: 12px;
