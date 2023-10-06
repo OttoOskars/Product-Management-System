a7b26500df539bf01f23acd1eec2f618c5dd06cc
Otto Oskars Āboliņš <ottooskarsabolins@gmail.com>
2023-10-06 17:22:21 +0300

    Added migrations for the database

---
+++ database/migrations/2023_10_06_133529_create_followers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('followers', function (Blueprint $table) {
+            $table->unsignedBigInteger('FollowerID');
+            $table->unsignedBigInteger('FollowingID');
+            $table->foreign('FollowerID')->references('UserID')->on('users');
+            $table->foreign('FollowingID')->references('UserID')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('followers');
+    }
+};
