4fb2104b6e2dc11e048b7a491c5647df05ff8f85
Otto Oskars Āboliņš <ottooskarsabolins@gmail.com>
2023-10-16 16:17:28 +0300

    login works

--- app/Http/Controllers/API/TweetController.php
+++ app/Http/Controllers/API/TweetController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Post;
+use App\Models\Tweet;
 
 class TweetController extends Controller
 {
