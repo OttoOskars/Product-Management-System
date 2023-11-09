dc1505f6205a0211d11247ffc3993b7e65758280
Otto Oskars Āboliņš <ottooskarsabolins@gmail.com>
2023-11-09 16:54:45 +0200

    Fixed css for posts going out of border.

--- resources/js/components/explore/explore.vue
+++ resources/js/components/explore/explore.vue
@@ -3,7 +3,7 @@
         <div class="navbar">
             <NavBar></NavBar>
         </div>
-        <div class="explore_content">
+        <div class="content">
             <Explore></Explore>
         </div>
         <div class="search">
@@ -39,56 +39,4 @@ export default {
     flex-direction: row;
 
 }
-.navbar{
-    width: 3/12*100%;
-
-}
-.search{
-    height: 100%;
-    width:4/12*100%;
-
-}
-.explore_content{
-    height:100%;
-    width:5/12*100%;
-
-}
-
-@media (max-width: 1250px) {
-    .navbar{
-        width:100px;
-    }
-    .search{
-        width:4/12*100%
-    }
-    .explore_content{
-        flex-grow: 1;
-    }
-}
-@media (max-width: 1000px) {
-    .navbar{
-        width:100px;
-    }
-    .search{
-        display:none;
-    }
-    .explore_content{
-        flex-grow: 1;
-    }
-}
-@media (max-width: 500px) {
-    .navbar{
-        width:100%;
-        position:fixed;
-        bottom:0;
-        z-index:99;
-    }
-    .search{
-        display:none;
-    }
-    .explore_content{
-        width:100%
-    }
-}
-
 </style>
