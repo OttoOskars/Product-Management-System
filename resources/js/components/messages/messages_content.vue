d2eef85de93f10716b61f8e00967dc7ca112e2f7
Markuss Spricis <markuss5764@gmail.com>
2023-11-13 11:17:58 +0200

    responsive

--- resources/js/components/messages/messages_content.vue
+++ resources/js/components/messages/messages_content.vue
@@ -1,7 +1,15 @@
 <template>
     <div class="messages-container">
         <div class="messages-main">
-            <div class="title">Messages</div>
+            <div class="top-bar">
+                <button class="back-icon" @click="goBack">
+                    <ion-icon name="arrow-back-outline"></ion-icon>
+                </button>
+                <div class="messages-top" v-if="user">
+                    <p class="title">Messages</p>
+                    <p class="user-tag">{{ user.UserTag }}</p>
+                </div>
+            </div>
             <div class="main-text">Welcome to your inbox!</div>
             <div class="under-text">Drop a line, share posts and more with private conversations between you and others on X. </div>
             <button class="write-button" @click="TogglePopup('EditTrigger')">New message</button>
@@ -71,6 +79,9 @@ export default{
         }
     },
     methods: {
+        goBack() {
+            this.$router.go(-1);
+        },
         inputFocus() {
             this.isInputFocused = true;
         },
@@ -122,12 +133,67 @@ export default{
         box-sizing: border-box;
         border-right: 1px solid #2F3336;
         border-left: 1px solid #2F3336;
-        .title{
-            color:white;
-            padding:10px;
-            padding-left:20px;
-            font-size: 23px;
-            font-weight:bold;
+        .top-bar{
+            height:60px;
+            width:100%;
+            background-color:rgba($color: #000000, $alpha: 0.8);
+            border-bottom:solid 1px #2F3336;
+            position:sticky;
+            top:0;
+            z-index:9;
+            box-sizing: border-box;
+            backdrop-filter: blur(5px);
+            display: flex;
+            align-items: center;
+            padding:0 10px;
+            .back-icon{
+                display:flex;
+                align-items: center;
+                justify-content: center;
+                box-sizing: border-box;
+                width:40px;
+                height:40px;
+                border-radius: 50%;
+                border:none;
+                font-size:22px;
+                background:none;
+                color:white;
+                cursor:pointer;
+                transition: all 0.3s;
+                &:hover{
+                    background-color: rgba($color: #1a1a1a, $alpha: 1);
+                }
+            }
+            .messages-top{
+                height:100%;
+                width:auto;
+                display:flex;
+                flex-direction: column;
+                padding:10px;
+                padding: 10px 10px 0px 30px;
+                box-sizing: border-box;
+                .title{
+                    width:100%;
+                    height:50%;
+                    display:flex;
+                    align-items: center;
+                    justify-content: flex-start;
+                    box-sizing: border-box;
+                    font-weight: bold;
+                    font-size: 20px;
+                    margin:0;
+                    padding:0;
+                    color: white;
+                }
+                .user-tag{
+                    width:100%;
+                    height:50%;
+                    margin:0;
+                    padding:0;
+                    font-size: 15px;
+                    color:#6A6F74;
+                }
+            }
         }
         .main-text{
             color: white;
@@ -313,6 +379,7 @@ export default{
                     transition: all 0.3s;
                     &:hover{
                         background-color: #080808;
+                        border-radius: 50px;
                     }
                     .user-info{
                         display:flex;
@@ -348,6 +415,105 @@ export default{
                     }
                 }
             }
+        }
+    }
+}
+@media (max-width: 1000px){
+    .messages-container{
+        display: flex;
+        flex-direction: column;
+        .messages-main{
+            height: 50%;
+            padding-bottom: 0px;
+            border-right: 0px;
+            border-left: 0px;
+            .main-text{
+                margin-left:4%;
+            }
+            .under-text{
+                padding-left: 4%;
+            }
+            .write-button{
+                margin-left: 4%;
+            }
+        }
+        .messages-right{
+            height: 50%;
+            border-right: 0px;
+            border-left: 0px;
+            padding-bottom: 0px;
+            justify-content: left;
+            .right-text{
+                margin-top: 15%;
+                padding-left: 4%;
+                justify-content: left;
+            }
+            .right-under-text{
+                padding-left: 4%;
+                justify-content: left;
+            }
+            .right-write-button{
+                margin-left: 4%;
+            }
+        }
+    }
+}
+@media (max-width: 700px){
+    .messages-container{
+        display: flex;
+        flex-direction: column;
+        .messages-main{
+            height: 50%;
+            padding-bottom: 0px;
+            border-right: 0px;
+            border-left: 0px;
+            .main-text{
+                font-size: 26px;
+                margin-top: 20%;
+                margin-left:4%;
+            }
+            .under-text{
+                font-size: 13px;
+                padding-left: 4%;
+                margin-top: 12px;
+            }
+            .write-button{
+                width: 150px;
+                margin-top: 30px;
+                margin-left: 4%;
+                font-size: 14px;
+                height: 45px;
+            }
+        }
+        .messages-right{
+            height: 50%;
+            border-right: 0px;
+            border-left: 0px;
+            padding-bottom: 0px;
+            justify-content: left;
+            .right-text{
+                font-size: 26px;
+                margin-top: 20%;
+                padding-left: 4%;
+                justify-content: left;
+            }
+            .right-under-text{
+                font-size: 13px;
+                padding-left: 4%;
+                padding-right: 4%;
+                margin-top: 12px;
+                justify-content: left;
+            }
+            .right-write-button{
+                width: 150px;
+                margin-top: 30px;
+                margin-left: 4%;
+                font-size: 14px;
+                height: 45px;
+            }
+        }
+        .edit-popup{
+            width: 100%;
         }
     }
 }
